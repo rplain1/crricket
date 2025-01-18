b4a48a0fffabb01b57610cda94bb621c00ce26d0
rplain1 <ryan.plain1@gmail.com>
2025-01-18 12:42:18 -0700

    split data and simple poisson model

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -71,6 +71,83 @@ transform_data <- function(data_list) {
   df
 }
 
+split_data <- function(df) {
+  checkmate::check_data_frame(df)
+  n <- nrow(df)
+
+  train_idx <- sample(1:n, size = 0.6 * n)  # 60% for training
+  remaining_idx <- setdiff(1:n, train_idx)
+  val_idx <- sample(remaining_idx, size = 0.2 * n)  # 20% for validation
+  test_idx <- setdiff(remaining_idx, val_idx)  # Remaining 20% for testing
+
+  df_transformed[, -which(colnames(df_transformed) == 'team')]
+
+  # Split the data
+  train_data <- df[train_idx, ]
+  val_data <- df[val_idx, ]
+  test_data <- df[test_idx, ]
+
+  # Check sizes
+  message("Training data: ", nrow(train_data))  # 60 rows
+  message("Validation data: ", nrow(val_data))    # 20 rows
+  message("Testing data: ", nrow(test_data))   # 20 rows
+
+  return(
+    list(
+      train_data = train_data,
+      val_data = val_data,
+      test_data = test_data
+    )
+  )
+}
+
+poisson_model <- function(df) {
+  glm(runs ~ ., data = df, family = poisson)
+}
+
+model_data <- function(data_splits) {
+  checkmate::assert_list(data_splits)
+
+  train_data <- data_splits$train_data
+  val_data <- data_splits$val_data
+  test_data <- data_splits$test_data
+
+  purrr::map(.x = data_splits, .f = poisson_model)
+
+  # TODO: wrap into a dataframe or list that contains eval metric and data
+}
+
+
+
+df_transformed <- transform_data(datasets2)
+data_splits <- split_data(df_transformed)
+
+
+df_transformed
+
+mod <- glm(runs ~ ., data = df_transformed[, -which(colnames(df_transformed) == 'team')], family = poisson)
+
+df_transformed |>
+  dplyr::mutate(
+    pred = predict(mod, newdata = df_transformed, type = 'response')
+  ) |>
+  dplyr::pull(pred) |> summary()
+
+df_transformed
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
